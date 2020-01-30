099f8277442f7e4a49bc42cf3200e89bfc3ae4f2
Avijit Das <adavijit@yahoo.com>
2020-01-30 18:00:08 +0530

    tour-of-heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes'
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero : Hero;
+  heroes = HEROES;
+  constructor() { }
+  onSelect(hero : Hero){
+    this.selectedHero = hero;
+  }
+
+  ngOnInit() {
+  }
+
+}
